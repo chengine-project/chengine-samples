5e71557efb3258ff424f73862e92121843beaf60
ilya.mikheev <ilya.mikheev@aliexpress.ru>
2021-03-24 21:26:03 +0300

    fixed grammar

--- telegram-corona-virus-bot/src/main/kotlin/com.coronavirus/Application.kt
+++ telegram-corona-virus-bot/src/main/kotlin/com.coronavirus/Application.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 /**
- * Don't forget to fill you's bot credentials in application.yml file
+ * Don't forget to fill your bot credentials in application.yml file
  *
  * If you don't know how to create telegram bot, please visit
  * https://core.telegram.org/bots#3-how-do-i-create-a-bot
